01b769b592e4701a14d6ab3dfbd2a62d0a87d7a7
Marco Avila <mavila6@email.arizona.com>
2021-07-17 07:47:43 -0700

    finished functionality for portfolio generation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -59,71 +59,71 @@ const promptProject = portfolioData => {
     return inquirer
         .prompt([
         {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of your project? (Required)',
-        validate: nameInput => {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project? (Required)',
+            validate: nameInput => {
             if (nameInput) {
                 return true;
             } else {
                 console.log('You need to enter a project name!');
                 return false;
             }
-        }
+            }
         },
         {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of the project (Required)',
-        validate: descriptionInput => {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
             if (descriptionInput) {
                 return true;
             } else {
                 console.log('You need to enter a project description!');
                 return false;
             }
-        }
+            }
         },
         {
-        type: 'checkbox',
-        name: 'languages',
-        message: 'What did you this project with? (Check all that apply)',
-        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
         },
         {
-        type: 'input',
-        name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)',
-        validate: linkInput => {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
             if (linkInput) {
                 return true;
             } else {
                 console.log('You need to enter a project GitHub link!');
                 return false;
             }
-        }
+            }
         },
         {
-        type: 'confirm',
-        name: 'feature',
-        message: 'Would you like to feature this project?',
-        default: false
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
         },
         {
-        type: 'confirm',
-        name: 'confirmAddProject',
-        message: 'Would you like to enter another project?',
-        default: false
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
         }
-    ])
-    .then(projectData => {
+        ])
+        .then(projectData => {
         portfolioData.projects.push(projectData);
         if (projectData.confirmAddProject) {
             return promptProject(portfolioData);
         } else {
             return portfolioData;
         }
-    });
+        });
 };
 
 promptUser()
